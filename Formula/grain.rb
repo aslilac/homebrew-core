e9ff4aba78a3e0c6e0314fcc5893527ae005b41f
McKayla Washburn <mckayla@hey.com>
2022-06-26 19:24:30 -0600

    Upgrade to 0.5.1

--- Formula/grain.rb
+++ Formula/grain.rb
@@ -1,18 +1,17 @@
+require "language/node"
+
 class Grain < Formula
   desc "Compiler toolchain and CLI for the Grain programming language. 🌾"
   homepage "https://grain-lang.org/"
-  url "https://github.com/grain-lang/grain/archive/refs/tags/grain-v0.4.7.tar.gz"
-  sha256 "849ec27beb0dc963284e10800fb7bb3bd87d4c83322effb8dd8df9bfe619edb9"
+  url "https://github.com/grain-lang/grain/archive/refs/tags/grain-v0.5.1.tar.gz"
+  sha256 "25fc6107aef537c4cdc9889e29f5b019f44509a19035dd38419ee078cc335014"
   license "LGPL-3.0-only"
 
-  depends_on "node@14" => :build
-  depends_on "yarn" => :build
+  depends_on "node@16" => :build
 
   def install
-    system "yarn"
-    system "yarn", "compiler", "build"
-    system "yarn", "compiler", "build:js"
-    system "yarn", "cli", "build-pkg"
+    system "npm", "install", "--ignore-scripts", *Language::Node.local_npm_install_args
+    system "npm", "run", "compiler", "build"
 
     cp "./pkg/grain", bin/"grain"
     chmod 755, bin/"grain"
@@ -20,9 +19,9 @@ class Grain < Formula
 
   test do
     (testpath/"test.gr").write <<~EOS
-      print("Hello!")
+      print("Hello Homebrew!")
     EOS
     
-    assert_match "Hello!\n", shell_output("#{bin}/grain #{testpath}/test.gr")
+    assert_match "Hello Homebrew!\n", shell_output("#{bin}/grain #{testpath}/test.gr")
   end
 end
